44a9e626847b9fd24d2c1cbb56f02152b0a4a583
Andries de Kock <andries.de@gmail.com>
2023-12-08 22:00:39 +0200

    Uncommented changes in a file

--- aoc-2023/Program.cs
+++ aoc-2023/Program.cs
@@ -3,17 +3,17 @@ using day02 = aoc_2023.day02;
 using day03 = aoc_2023.day03;
 using day04 = aoc_2023.day04;
 
-// // day 1
-// day01.PartOne.Process();
-// day01.PartTwo.Process();
+// day 1
+day01.PartOne.Process();
+day01.PartTwo.Process();
 
-// // day 2
-// day02.PartOne.Process();
-// day02.PartTwo.Process();
+// day 2
+day02.PartOne.Process();
+day02.PartTwo.Process();
 
-// // day 3
-// day03.PartOne.Process();
-// day03.PartTwo.Process();
+// day 3
+day03.PartOne.Process();
+day03.PartTwo.Process();
 
 // day 4
 day04.PartOne.Process();
