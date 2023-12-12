49225ffd357b85094f40bfdb14abef183f4630f9
AndriesDK <andriesd@cashcrusaders.co.za>
2023-12-12 11:41:03 +0200

    Fix FilePath method in Extractor.cs and optimize calculation in PartOne.cs

--- aoc-2023/Extractor.cs
+++ aoc-2023/Extractor.cs
@@ -9,7 +9,7 @@ public static class Extractor
     private static bool IsDebuggerAttached() => Debugger.IsAttached;
 
     private static string GetFilePath(string path) =>
-        IsDebuggerAttached() ? $"{FileSystem.FilePath}{path}" : path;
+        IsDebuggerAttached() ? $"{FileSystem.FilePath()}{path}" : path;
 
     public static FileStream ExtractUsingFileStream(string path) =>
         File.OpenRead(GetFilePath(path));
--- aoc-2023/day06/PartOne.cs
+++ aoc-2023/day06/PartOne.cs
@@ -16,56 +16,37 @@ public static partial class PartOne
         // we create a new array to store the number of ways to win for each time
         var output = new int[times.Length];
 
-        /*
-        NOTE
-            - https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.zip?view=net-5.0
+        for (int i = 0; i < times.Length; i++)
+        {
+            // we get the current time
+            var time = times[i];
 
-            We zip our times and distances together so we can iterate over them at the same time,
-            we then iterate over the zipped list and calculate the number of ways to win.
+            // we create a new array to store the total milliseconds
+            var total = Array.Empty<int>();
 
-            We then add the number of ways to win to our output array and repeat the process
-            until we have iterated over all the values in our times array.
-
-            We then multiply all the values in our output array to get our final result
-         */
-        times
-            .Zip(distances) // zip times and distances together
-            .ToList()
-            .ForEach(x => // iterate over the zipped list
+            // we start from 0 milliseconds and count up to the recorded time (times[i])
+            for (int k = 0; k < time; k++)
             {
-                // we create a new array to store the total milliseconds
-                var total = Array.Empty<int>();
+                /*
+                NOTE
+                    We calculate the number of milliseconds the button was pressed.
+                    e.g total recorded time (time - current time (k)) * current time (k)
 
-                // we start from 0 milliseconds and count up to the recorded time (x.First)
-                for (int i = 0; i < x.First; i++)
+                    e.g (7 - 0) * 0 = 0
+                    e.g (7 - 1) * 1 = 6
+                    e.g (7 - 2) * 2 = 10
+                 */
+
+                if (((time - k) * k) > distances[i])
                 {
-                    /*
-                    NOTE
-                        Calculate the number of milliseconds the button was pressed.
-                        e.g total recorded time ((x.First) - current time (i)) * current time (i)
-                        e.g (7 - 0) * 0 = 0
-                        e.g (7 - 1) * 1 = 6
-                        e.g (7 - 2) * 2 = 10
-
-                        If the the result is greater than the total distance recorded (x.Second)
-                        then we add the current time (i) to our total array
-                     */
-
-                    // Console.WriteLine($"[Pressed: {i}, Traveled: {(x.First - i) * i}]");
-
-                    var millisecondsHeld = (x.First - i) * i;
-                    var distance = x.Second;
-
-                    if (millisecondsHeld > distance)
-                    {
-                        total = total.Append(i).ToArray();
-                    }
+                    // if the number of milliseconds is greater than the distance, we append it to the total array
+                    total = total.Append(k).ToArray();
                 }
+            }
 
-                // count total number of ways to win
-                output[Array.IndexOf(times, x.First)] = total.Length;
-                // Console.WriteLine($"Total ways to win: {total.Length}");
-            });
+            // count total number of ways to win
+            output[i] = total.Length;
+        }
 
         Console.WriteLine(output.Aggregate((x, y) => x * y));
     }
