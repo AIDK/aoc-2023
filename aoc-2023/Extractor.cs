c8c01df8287dd78328a184e104861a82937ab557
AndriesDK <andriesd@cashcrusaders.co.za>
2023-12-12 07:55:06 +0200

    Update file paths in Extractor.cs

--- aoc-2023/Extractor.cs
+++ aoc-2023/Extractor.cs
@@ -9,7 +9,7 @@ public static class Extractor
     private static bool IsDebuggerAttached() => Debugger.IsAttached;
 
     private static string GetFilePath(string path) =>
-        IsDebuggerAttached() ? @$"E:\Repository\Github\Projects\aoc-2023\aoc-2023\{path}" : path;
+        IsDebuggerAttached() ? $"{FileSystem.FilePath}{path}" : path;
 
     public static FileStream ExtractUsingFileStream(string path) =>
         File.OpenRead(GetFilePath(path));
